bf8e1b8ab7a2622a1850506fe74c3bae19660fde
nmyza <nazar.myza@gmail.com>
2018-01-18 03:56:07 +0200

    Three type of run Spring application

--- src/main/java/org/brocom/spring/boot/Main.java
+++ src/main/java/org/brocom/spring/boot/Main.java
@@ -1,12 +1,31 @@
 package org.brocom.spring.boot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        runThird(args);
+    }
+
+    private static void runFirst(String... args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    private static void runSecond(String... args) {
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+    private static void runThird(String... args) {
+        new SpringApplicationBuilder()
+                .sources(Main.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
     }
 }
